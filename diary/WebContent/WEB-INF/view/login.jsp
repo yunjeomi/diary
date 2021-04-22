81540db20ca7f930dc8d95371ae55d99598384f1
yunjeong <jjeongsla@gmail.com>
2021-04-22 14:48:31 +0900

    로그인/로그아웃 기능 구현

--- diary/WebContent/WEB-INF/view/login.jsp
+++ diary/WebContent/WEB-INF/view/login.jsp
@@ -7,6 +7,23 @@
 <title>login</title>
 </head>
 <body>
-	<h1>login</h1>
+	<!-- 로그인 전; 로그인 안 되어있을 때 -->
+	<c:if test="${sessionMember == null}">
+		<h1>login</h1>
+		<form action="${pageContext.request.contextPath}/login" method="post">
+			<div>
+				<div>ID <input type="text" name="memberId"></div>
+				<div>PW <input type="password" name="memberPw"></div>
+				<div><button type="submit">로그인</button></div>
+			</div>
+		</form>
+	</c:if>
+	
+	<!-- 로그인 후; 로그인 되어있을 때 -->
+	<c:if test="${sessionMember != null}">
+		<div>${sessionMember.memberId}님 반갑습니다.</div>
+		<div><a href="${pageContext.request.contextPath}/auth/">다이어리</a></div>
+		<div><a href="${pageContext.request.contextPath}/auth/logout">로그아웃</a></div>
+	</c:if>
 </body>
 </html>
--- diary/src/gdu/diary/controller/LoginController.java
+++ diary/src/gdu/diary/controller/LoginController.java
@@ -1,18 +1,49 @@
 package gdu.diary.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import gdu.diary.service.MemberService;
+import gdu.diary.vo.Member;
 
 
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
-	
+	private MemberService memberService;
+	//로그인 폼
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
 	}
-
+	
+	//로그인 액션
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.memberService = new MemberService();
+		
+		String memberId = request.getParameter("memberId");
+		String memberPw = request.getParameter("memberPw");
+		
+		Member member = new Member();
+		member.setMemberId(memberId);
+		member.setMemberPw(memberPw);
+		System.out.println("member-> "+member);
+		
+		Member returnMember = this.memberService.getMemberByKey(member);
+		if(returnMember == null) {
+			System.out.println("*로그인 실패*");
+		} else {
+			HttpSession session = request.getSession();	//세션을 우선 불러온다.
+			session.setAttribute("sessionMember", returnMember);	//불러오고 값을 설정한다.
+			System.out.println("*로그인 성공*");
+		}
+		
+		response.sendRedirect(request.getContextPath()+"/login");
+	}
 }
